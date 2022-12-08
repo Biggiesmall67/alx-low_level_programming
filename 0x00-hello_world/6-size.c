aa96872d66f43b451ee781e2a4a364ad9779a7b4
Biggie Small <isaacike67@gmail.com>
2022-12-08 10:07:43 -0800

    Hello World C Project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,19 @@
 #incude <stdio.h>
 
+/**                                                                                                               
+ * Write a C program that prints the size of various types on the computer it is compiled and ru
+ * You should produce the exact same output as in the example
+ * Warnings are allowed                                                                      
+ * Your program should return 0                 
+ */
+
 int main (void)
 
-	/**
-	 * Write a C program that prints the size of various types on the computer it is compiled and run on.
-	 * You should produce the exact same output as in the example
-	 * Warnings are allowed
-	 * Your program should return 0
-	 */
-
 {
-	printf("Size of char: %d byte(s)\n", sizeof(char));
-	printf("Size of int: %d byte(s)\n", sizeof(int));
-	printf("Size of long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of float: %d byte(s)\n", sizeof(float));
-	
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
