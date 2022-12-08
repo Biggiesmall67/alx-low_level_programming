eb45e1525c4f081ead25e8e55924377114e5054c
Biggie Small <isaacike67@gmail.com>
2022-12-08 12:26:24 -0800

    Hello World C Project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/**                                                                                                                       
- * main - print the phrase "Programming is like building a multilingual puzzle                                         
- * Return: 0 if exited properly, non-zero otherwise
+/**
+ *main - print a string
+ *Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
