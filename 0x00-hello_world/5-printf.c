481182e40fddfd4c3a2a46ac80d08be0007fb5ac
Biggie Small <isaacike67@gmail.com>
2022-12-08 10:56:49 -0800

    Hello World C Project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 
+/**                                                                                                                       
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */       
 int main(void)
-
-/**
-*Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
-Use the function printf
-
-*You are not allowed to use the function puts
-
-*Your program should return 0
-
-*Your program should compile without warning when using the -Wall gcc option
-*/
-
 {
-	printf("with proper grammar, but the outcome isa piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
