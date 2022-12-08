3fce4643440b9dc375621735c9d9e495819aab11
Biggie Small <isaacike67@gmail.com>
2022-12-08 10:23:25 -0800

    Hello World C Project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * print the size of various data types
- * program should return 0
+ * main - prints the size of various types
+ * return: 0 if exited properly, non zero otherwise
  */
 int main (void)
-
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
