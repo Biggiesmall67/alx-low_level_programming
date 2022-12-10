c537918883d8d984a10eb89abcea9fccf90df240
Biggie Small <isaacike67@gmail.com>
2022-12-09 21:42:24 -0800

    C Project, If, Else, While

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,12 +13,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("last digit of %d is %d ", n, n % 10);
-	if (n % 10 > 5)
+	printf("last digit of %d is %d ", n, n % 9);
+	if (n % 9 > 5)
 		printf("and is greater than 5\n");
-	else if (n % 10 == 0)
+	else if (n % 9 == 0)
 		printf("and is 0\n");
-	else if (n % 10 < 6 && n % 10 != 0)
+	else if (n % 9 < 6 && n % 9 != 0)
 		printf("and is less than 6 and not 0\n");
 	return (0);
 }
