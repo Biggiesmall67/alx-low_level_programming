a8788a51fac3d19c7dac1d28530d3bf07953add1
Biggie Small <isaacike67@gmail.com>
2023-01-05 22:57:45 -0800

    Recursion Project

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,37 +1,18 @@
 #include "main.h"
 
 /**
- * power_operation - returns the natural square root of a number.
- * @n: input number.
- * @c: iterator.
- * Return: square root or -1.
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
  */
 
-int power_operation(int n, int c)
+void _puts_recursion(char *s)
 {
-	if (c % (n / c) == 0)
+	if (*s != '\0')
 	{
-	if (c * (n / c) == n)
-	return (c);
-	else
-	return (-1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 	}
-	return (0 + power_operation(n, c + 1));
-}
-
-/**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: input number.
- * Return: natural square root.
- */
-
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-	return (-1);
-	if (n == 0)
-	return (0);
-	if (n == 1)
-	return (1);
-	return (power_operation(n, 2));
+	else
+	_putchar('\n');
 }
